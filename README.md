74cd9f9312b86f410ae1ed1fbb7194f92e6e6f95
wsyski <wsyski@gmail.com>
2022-04-04 18:43:07 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-lazy-load-portlet
+
+React Portlet
---
+++ src/AppComponent.js
@@ -0,0 +1,27 @@
+import React, { Suspense } from "react";
+import styled from "styled-components";
+import Loader from "./Loader";
+import ErrorBoundary from "./ErrorBoundary";
+import LazyCalendar from "./LazyCalendar";
+
+const Header = styled.h1`
+  position: absolute;
+  width: 100%;
+  margin: 100px auto;
+`;
+
+export default function AppComponent() {
+	return (
+		<div className="App">
+			<ErrorBoundary>
+				<Header>Calendar</Header>
+
+				<Suspense fallback={<Loader />}>
+					<div className="calendar-container">
+						<LazyCalendar />
+					</div>
+				</Suspense>
+			</ErrorBoundary>
+		</div>
+	);
+}
