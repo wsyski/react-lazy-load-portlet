d0908b13f4700ceffc0e23d46d64742fa6505a2f
wsyski <wsyski@gmail.com>
2022-11-06 20:12:22 +0100

    Upgrade to Liferay 7.4.3.49

--- src/LazyCalendar.js
+++ src/LazyCalendar.js
@@ -1,6 +1,13 @@
 import React from "react";
 import LiferayUtil from "./LiferayUtil";
 
-const LazyCalendar = React.lazy(() => LiferayUtil.isPortal() ? import("./Calendar") : import("./Calendar"));
+const LazyCalendar = LiferayUtil.isPortal()
+    ? new Promise((resolve, reject) => Liferay.Loader.require(["react-lazy-load-portlet@1.0.0/Calendar"],
+        (Module) => {
+            console.log('Module', Module);
+            resolve(new Module())
+        },
+        (error) => reject(error)))
+    : React.lazy(() => import("./Calendar"));
 
 export default LazyCalendar;
