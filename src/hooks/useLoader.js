4f44baafb1bb0cb9bdf02fbfb60112aa29b376df
wsyski <wsyski@gmail.com>
2022-11-06 21:48:35 +0100

    Upgrade to Liferay 7.4.3.49

--- src/hooks/useLoader.js
+++ src/hooks/useLoader.js
@@ -4,38 +4,35 @@ import {useCallback, useRef} from 'react';
 const EmptyModule = {default: () => null};
 
 export default function useLoader() {
-	const modulesRef = useRef(new Map());
-	const isMounted = useIsMounted();
+    const modulesRef = useRef(new Map());
+    const isMounted = useIsMounted();
 
-	return useCallback(
-		(module) => {
-			if (!modulesRef.current.has(module)) {
-				modulesRef.current.set(
-					module,
-					new Promise((resolve) => {
-						Liferay.Loader.require(
-							[module],
-							(Module) => {
-								if (isMounted()) {
-									resolve(Module ? Module : EmptyModule);
-								}
-							},
-							(error) => {
-								if (process.env.NODE_ENV === 'development') {
-									console.error(error);
-								}
+    return useCallback(
+        (module) => {
+            if (!modulesRef.current.has(module)) {
+                modulesRef.current.set(
+                    module,
+                    new Promise((resolve) => {
+                        Liferay.Loader.require(
+                            [module],
+                            (Module) => {
+                                if (isMounted()) {
+                                    resolve(Module ? Module : EmptyModule);
+                                }
+                            },
+                            (error) => {
+                                console.error(error);
+                                if (isMounted()) {
+                                    resolve(EmptyModule);
+                                }
+                            }
+                        );
+                    })
+                );
+            }
 
-								if (isMounted()) {
-									resolve(EmptyModule);
-								}
-							}
-						);
-					})
-				);
-			}
-
-			return modulesRef.current.get(module);
-		},
-		[isMounted]
-	);
+            return modulesRef.current.get(module);
+        },
+        [isMounted]
+    );
 }
